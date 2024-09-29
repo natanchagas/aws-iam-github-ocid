45ddeaa000576e4fe78a34971a2a736fe59a9a0e
Natan das Chagas Silva <natanchagas@Natan-Mac-Air.local>
2024-09-29 17:48:25 -0300

    chore: terraform fmt

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,18 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "5.69.0"
-      }
-    }
-
-    backend "s3" {
-        bucket         = "natanchagas-aws-iam-github-ocid"
-        key            = "terraform.tfstate"
-        region         = "us-east-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.69.0"
     }
   }
 
-  provider "aws" {
+  backend "s3" {
+    bucket = "natanchagas-aws-iam-github-ocid"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
